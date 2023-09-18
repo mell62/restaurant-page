2e5b01bc66eb43155524fb4b6a7de985ac2b6266
Mridhul <mridhul67@gmail.com>
2023-09-18 20:49:07 +0530

    Set up normalize.css

--- webpack.config.js
+++ webpack.config.js
@@ -10,8 +10,16 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
         test: /\.s[ac]ss$/i,
         use: ["style-loader", "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
       },
     ],
   },
