bc19c1bec1fea4be33174bed31f4f0bd96d9e948
Mridhul <mridhul67@gmail.com>
2024-05-11 19:19:51 +0530

    Import and run home function

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import './style.css';
 import './modern-normalize.css';
 import { fillHero, fillButtons, styleButtons, activateHome } from './barrel.js';
+import { fillHome } from './barrel.js';
 
 fillHero();
 fillButtons();
 activateHome();
 styleButtons();
+fillHome();
