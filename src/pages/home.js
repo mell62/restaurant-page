1cfc9de3c0e29d86719aa1b95f2178ab7f091d0c
Mridhul <mridhul67@gmail.com>
2024-05-11 23:58:15 +0530

    Add an image to home page hero

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,3 +1,4 @@
+import bobaImg from "../images/boba.png";
 export { fillHome };
 
 const content = document.querySelector("#content");
@@ -5,26 +6,33 @@ const content = document.querySelector("#content");
 function fillHome() {
   const homeContainer = document.createElement("div");
   const homeHero = document.createElement("div");
+  const homeHeroText = document.createElement("div");
   const homeHeroLogo = document.createElement("div");
   const homeHeroLogoThe = document.createElement("div");
   const homeHeroLogoBobaCube = document.createElement("div");
   const homeText = document.createElement("div");
+  const bobaIcon = document.createElement("img");
 
-  homeHero.textContent = "Welcome to";
+  homeHeroText.textContent = "experience";
   homeHeroLogoThe.textContent = "the";
   homeHeroLogoBobaCube.textContent = "boba cube.";
   homeText.textContent =
     " Indulge in the perfect blend of flavors and fun at our cafe. Sip, relax, and let the good times bubble up!";
+  bobaIcon.src = bobaImg;
+  bobaIcon.setAttribute("style", "width:2rem;");
 
   homeHeroLogo.classList.add("home-hero-logo");
   homeHeroLogoThe.classList.add("home-hero-logo-the");
+  homeHeroLogoBobaCube.classList.add("home-hero-logo-bobacube");
   homeHero.classList.add("home-hero");
   homeContainer.classList.add("home-container");
   homeText.classList.add("home-text");
 
   homeHeroLogo.appendChild(homeHeroLogoThe);
   homeHeroLogo.appendChild(homeHeroLogoBobaCube);
+  homeHeroLogo.appendChild(bobaIcon);
   homeHero.appendChild(homeHeroLogo);
+  homeHero.appendChild(homeHeroText);
   homeContainer.appendChild(homeHero);
   homeContainer.appendChild(homeText);
   content.appendChild(homeContainer);
