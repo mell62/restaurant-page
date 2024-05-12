2d346a43fd33a1f345925050234c3f208edef6db
Mridhul <mridhul67@gmail.com>
2024-05-12 19:35:24 +0530

    Import and run offer card function

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
-import './style.css';
-import './modern-normalize.css';
-import { fillHero, fillButtons, styleButtons, activateHome } from './barrel.js';
-import { fillHome } from './barrel.js';
+import "./style.css";
+import "./modern-normalize.css";
+import {
+  fillHero,
+  fillButtons,
+  styleButtons,
+  activateHome,
+  fillHome,
+  fillHomeOffer,
+} from "./barrel.js";
 
 fillHero();
 fillButtons();
 activateHome();
 styleButtons();
 fillHome();
+fillHomeOffer();
