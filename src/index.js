c585b1bb44ae9eb32095b3a6b3aef47612dc683c
Mridhul <mridhul67@gmail.com>
2024-05-13 00:42:56 +0530

    Implement logic to wipe content when clicking other nav buttons

--- src/index.js
+++ src/index.js
@@ -15,3 +15,40 @@ activateHome();
 styleButtons();
 fillHome();
 fillHomeOffer();
+
+const nav = document.querySelector("nav");
+const navButtons = nav.querySelectorAll("button");
+const content = document.querySelector("#content");
+
+const homeBtn = Array.from(navButtons).filter(
+  (btn) => btn.textContent === "HOME"
+);
+const menuBtn = Array.from(navButtons).filter(
+  (btn) => btn.textContent === "MENU"
+);
+const contactBtn = Array.from(navButtons).filter(
+  (btn) => btn.textContent === "CONTACT"
+);
+
+homeBtn[0].addEventListener("click", () => {
+  const homeContainer = document.querySelector(".home-container");
+  if (document.querySelector(".home-container")) {
+    content.removeChild(homeContainer);
+  }
+  fillHome();
+  fillHomeOffer();
+});
+
+menuBtn[0].addEventListener("click", () => {
+  const homeContainer = document.querySelector(".home-container");
+  if (document.querySelector(".home-container")) {
+    content.removeChild(homeContainer);
+  }
+});
+
+contactBtn[0].addEventListener("click", () => {
+  const homeContainer = document.querySelector(".home-container");
+  if (document.querySelector(".home-container")) {
+    content.removeChild(homeContainer);
+  }
+});
