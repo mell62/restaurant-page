789c7f5d73025346fa3a68f539bf335b94532013
Mridhul <mridhul67@gmail.com>
2024-05-19 03:05:58 +0530

    Export menu item generation function

--- src/barrel.js
+++ src/barrel.js
@@ -1,2 +1,3 @@
 export { fillHero, fillButtons, styleButtons, activateHome } from "./base.js";
 export { fillHome, fillHomeOffer } from "./pages/home.js";
+export { fillMenuCards } from "./pages/menu.js";
