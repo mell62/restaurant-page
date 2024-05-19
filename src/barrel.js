89f868eff4b0481327ec13aa65c9577885900caa
Mridhul <mridhul67@gmail.com>
2024-05-19 22:52:48 +0530

    Export function to fill menu hero

--- src/barrel.js
+++ src/barrel.js
@@ -1,3 +1,3 @@
 export { fillHero, fillButtons, styleButtons, activateHome } from "./base.js";
 export { fillHome, fillHomeOffer } from "./pages/home.js";
-export { fillMenuCards } from "./pages/menu.js";
+export { fillMenuCards, fillMenuHero } from "./pages/menu.js";
