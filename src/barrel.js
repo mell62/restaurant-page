05024a674f08c96ff9f5225dd9676d90acae1789
Mridhul <mridhul67@gmail.com>
2024-06-01 21:03:18 +0530

    Export function to generate hero in contact page

--- src/barrel.js
+++ src/barrel.js
@@ -1,3 +1,4 @@
 export { fillHero, fillButtons, styleButtons, activateHome } from "./base.js";
 export { fillHome, fillHomeOffer } from "./pages/home.js";
 export { fillMenuCards, fillMenuHero } from "./pages/menu.js";
+export { fillContactHero } from "./pages/contact.js";
